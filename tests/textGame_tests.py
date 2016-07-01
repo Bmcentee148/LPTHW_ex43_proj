baf0010a8425d310a1bdf9bcbee31c3f5b3092ee
Brian McEntee <brian.m.mcentee@gmail.com>
2016-07-01 19:20:09 -0400

    Finish first round of class coding and unit testing with nosetests.
    Still need tests for GameEngine class
    Still need Docstrings for GameEngine class
    Next: finish GameEngine class and unit tests for it

--- tests/textGame_tests.py
+++ tests/textGame_tests.py
@@ -1,13 +1,35 @@
 # Simple skeleton file for running tests on our project
 
 from nose.tools import *
-import textGame
+from textGame.textGame import *
+# from textGame.textGame import Scene
+# from textGame.textGame import Death
+# from textGame.textGame import Scene_Map
 
-def setup() :
-    print 'SETUP!'
+def test_scene_base() :
+    # We are not able to directly create an object of type Scene
+    assert_raises(NotImplementedError, Scene)
 
-def teardown() :
-    print 'TEARDOWN!'
+def test_subclassing() :
+    # Test we can create an object from subclass of Scene and call its methods
+    d = Death()
+    assert_equal(d.enter(), None)
+    e = Escape_Pod()
+    assert_equal(e.enter(), None)
 
-def test_basic() :
-    print 'I RAN!'
+def test_scene_map() :
+    scene_map = Scene_Map('armory')
+    assert_equal(scene_map.start_scene, 'armory')
+    assert_equal(Scene_Map(None).start_scene, None)
+    assert_not_equal(scene_map.start_scene, 'corridor')
+    assert_equal(scene_map.get_scene('vacant'), None)
+    assert_equal(Scene_Map(None).get_opening_scene(), None)
+
+    assert_equal(scene_map.get_opening_scene(), Armory())
+    assert_equal(scene_map.get_scene('death'), Death())
+
+def test_gameEngine() :
+    pass
+
+
+
--- textGame/textGame.py
+++ textGame/textGame.py
@@ -3,48 +3,91 @@
 Classes:
     Scene - The base class for each scene in the game
     Scene_Map - A map that will hold determine which scene is displayed next
-    Engine - A game engine that is responsible for processing a users game
+    GameEngine - A game engine that is responsible for processing a users game
 """
 
 #-- Import Statements --#
 from random import randint
+import sys
 
 #-- Class Definitions --#
 class Scene(object) :
     """A base class for each scene in the game."""
 
+    # Prevent creation of instance objects of type Scene
+    def __init__(self) :
+        raise NotImplementedError
+
     # Abstract method that will be implemented in all subclasses
     def enter(self) :
-        """Not implemented. Subclass and provide functionality."""
-        raise NotImplementedError
+        """Not implemented. Subclass and provide functionality.
+
+        Returns:
+            the name of the next scene in the players path 
+        """
+        pass
+
+    def __eq__(self, other) :
+        # Just check if type of Scene are the same.
+        return type(self) == type(other)
+
 
 class Death(Scene) :
     """---TODO---"""
-    pass
+    
+    def __init__(self) :
+        pass
+
+    def enter(self) :
+        pass
 
 
 class Central_Corridor(Scene) :
     """---TODO---"""
-    pass
+    
+    def __init__(self) :
+        pass
+
+    def enter(self) :
+        pass
     
 
 class Armory(Scene) :
     """---TODO---"""
-    pass
+    
+    def __init__(self) :
+        pass
+
+    def enter(self) :
+        pass
     
 
 class Escape_Pod(Scene) :
     """---TODO---"""
-    pass
+    
+    def __init__(self) :
+        pass
+
+    def enter(self) :
+        pass
     
 
 class Bridge(Scene) :
     """---TODO---"""
-    pass
     
+    def __init__(self) :
+        pass
+    
+    def enter(self) :
+        pass
 
 class Scene_Map(object) :
-    """A map of the game's scene layout structure."""
+    """A map of the game's scene layout structure.
+
+    Attributes:
+        scenes - a map of scene names to scenes
+        start_scene - the name of the scene to start from
+    """
 
     scenes = {
         'death' : Death(),
@@ -54,12 +97,62 @@ class Scene_Map(object) :
         'bridge' : Bridge()
     }
 
-
     def __init__(self, start_scene) :
         self.start_scene = start_scene
 
     def get_scene(self, scene_name) :
+        """Get the scene with the given name.
+
+        Arguments:
+            scene_name - the name of scene to retrieve
+        Returns:
+            a Scene object of given name if the name is in the map, else None 
+        """
         return Scene_Map.scenes.get(scene_name, None)
 
     def get_opening_scene(self) :
+        """Get the opening scene.
+
+        Returns:
+            the beginning Scene object
+        """
         return self.get_scene(self.start_scene)
+
+
+class GameEngine(object) :
+
+
+    def __init__(self, scene_map) :
+        self.scene_map = scene_map
+
+    def play(self) :
+        curr_scene = self.scene_map.get_opening_scene()
+
+        while True :
+            print('\n----------------------------')
+            next_scene_name = curr_scene.enter()
+            current_scene = self.scene_map.get_scene(next_scene_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
